0308586d5df057ff4a521ab3b5d7739077ef5ad2
Ario Tamaddoni <ariozone6@gmail.com>
2018-10-23 14:14:57 -0700

    Table class component created in table.js to render table elements.#1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import Start from './start'
-export default class App extends React.Component {
+import React, { Component, Fragment } from 'react'
+// import Start from './start'
+import Table from './table'
+export default class App extends Component {
   render() {
     return (
-      <Start/>
+      <Fragment>
+        {/* <Start/> */}
+        <Table/>
+      </Fragment>
+
     )
   }
 }
---
+++ src/table.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Table extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid"><div><img src="..." alt="..." className="rounded-circle"/></div>
+      </div>
+    )
+  }
+}
